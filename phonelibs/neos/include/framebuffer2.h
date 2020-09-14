2b60d20e828aff6cb568e34645eb7163b6548ef5
ihsakashi <yuvch122@gmail.com>
2020-09-14 08:19:28 -0600

    include neos headers

---
+++ phonelibs/neos/include/framebuffer2.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct FramebufferState FramebufferState;
+
+FramebufferState* framebuffer_init(
+    const char* name, int32_t layer, int alpha,
+    int *out_w, int *out_h);
+
+void framebuffer_swap(FramebufferState *s);
+void framebuffer_swap_layer(FramebufferState *s);
+
+#ifdef __cplusplus
+}
+#endif
